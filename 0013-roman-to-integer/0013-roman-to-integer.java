e08bf141d8748ec2e66b179fb853d072539c6070
Lashmini Anand <sec23ci058@sairamtap.edu.in>
2026-03-18 05:28:43 +0530

    Time: 3 ms (78.33%), Space: 46.3 MB (92.65%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int romanToInt(String s) {
+        int []val = new int[128];
+        val['I']=1;
+        val['V']=5;
+        val['X']=10;
+        val['L']=50;
+        val['C']=100;
+        val['D']=500;
+        val['M']=1000;
+        int sum = 0;
+        for(int i = 0; i<s.length(); i++){
+            int current = val[s.charAt(i)]; //ii i
+            if(i<s.length()-1 && current < val[s.charAt(i+1)]){
+                sum -= current;
+            }else{
+                sum += current;
+            }
+        }
+        return sum; 
+    } 
+}
