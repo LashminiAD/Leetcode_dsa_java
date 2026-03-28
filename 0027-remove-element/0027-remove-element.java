5ef95d17ac48528cc577d57a2cbf86f78faf8d80
Lashmini Anand <sec23ci058@sairamtap.edu.in>
2026-03-29 00:04:48 +0530

    Optimize removeElement method for better efficiency
    Refactor removeElement method to improve performance by using the last element for removal.

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -10,3 +10,23 @@ class Solution {
         return k;
     }
 }
+
+
+//OR ---> Order doesn’t matter (faster in practice)
+
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+        int n = nums.length;
+        while( k < n){
+            if(nums[k] == val){
+                nums[k] = nums[n-1];
+                n--;
+            }else{
+            k++;
+            }
+        }
+        return n;
+    }
+}
