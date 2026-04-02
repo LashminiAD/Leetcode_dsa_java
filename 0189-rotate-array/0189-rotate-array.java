ffd2e709a9c52fd2de9e6ac9c38d3b1b275dedb8
Lashmini Anand <sec23ci058@sairamtap.edu.in>
2026-04-02 21:02:45 +0530

    Clarify rotation logic with a comment
    Add comment to clarify the purpose of k = k % n.
    REV1

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,7 +1,7 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
-        k = k%n;
+        k = k%n; //remaining rotation
          reverse(nums, 0, n-1); //reverese all
          reverse(nums, 0, k-1); // 0 to k at first
          reverse(nums, k , n-1);// balance rev
