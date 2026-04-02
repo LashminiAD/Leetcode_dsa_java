c549c160e1545d567671810ebbea98c7e8ee8c2f
Lashmini Anand <sec23ci058@sairamtap.edu.in>
2026-04-02 22:18:16 +0530

    Enhance isPowerOfTwo method with comments
    Added comments explaining the logic for checking if a number is a power of two. REV1

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,6 +1,23 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n<=0) return false;
+        if(n<=0) return false;   //  If number is 0 or negative → not a power of 2
+
         return (n & (n-1)) == 0;
     }
 }
+
+/*         🔥 Magic Trick: n & (n - 1)
+
+         👉 This operation removes the last set (1) bit
+
+         ✅ Example: n = 8
+         n     = 1000
+         n - 1 = 0111
+         ----------------
+         &     = 0000 ✅        ✔ Result is 0 → only one '1' present   👉 So it is a power of 2
+
+         ❌ Example: n = 6
+         n     = 110
+         n - 1 = 101
+         ----------------
+         &     = 100 ❌        ✔ Not zero → more than one '1'  👉 Not a power of 2   */
