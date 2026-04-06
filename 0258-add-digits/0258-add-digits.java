fa36c657e0e34a21aa1dbb2152d8f52a1921ee44
Lashmini Anand <sec23ci058@sairamtap.edu.in>
2026-04-06 05:34:08 +0530

    Implement brute force digit sum calculation
    Added a brute force method to calculate the sum of digits until a single digit is obtained.

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -3,3 +3,28 @@ class Solution {
         if (num == 0) return 0; return 1 + (num - 1) % 9;
     }
 }
+
+//brute force
+
+import java.util.*;
+public class Main {
+  
+  static int countDig(int n){
+    while(n >=10){
+      int sum = 0;
+      
+      while(n > 0) {
+        sum += n%10; //38 -> 8 , + 3 => 11 - 1 , + 1 
+        n /=10; //11 = sum , = 2
+    }
+     n = sum; //11
+    }
+    return n;
+  }
+  
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      System.out.println(countDig(n));
+    }
+}
