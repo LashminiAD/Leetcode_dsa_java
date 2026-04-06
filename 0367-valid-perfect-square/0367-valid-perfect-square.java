c3f7f88eb090fcb01872bc825a34afc8eb8753a0
Lashmini Anand <sec23ci058@sairamtap.edu.in>
2026-04-06 05:57:51 +0530

    Implement perfect square check in Main class
    Added a new method to check if a number is a perfect square using a simple iterative approach.

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -2,8 +2,35 @@ class Solution {
     public boolean isPerfectSquare(int num) {
         long n = num;
         while(n*n > num){
-            n = (n + num/n)/2;
+            n = (n + num/n)/2; // (16 + 16/16) / 2 = 17/2
         }
         return n*n==num;
+    }
+}
+
+//or 
+
+import java.util.*;
+public class Main {
+  
+  static boolean perfSq(int n){
+    
+    int i = 1;
+    while(i*i <= n){ // 1<=16
+    if(i*i == n) return true;
+      i++;
+    }
+    return false;
+    
+  }
+
+
+
+
+    
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      System.out.println(perfSq(n));
     }
 }
