18c130fc12bead02ab6273da45766b306d1be3d6
Lashmini Anand <sec23ci058@sairamtap.edu.in>
2026-04-06 06:07:28 +0530

    Implement FizzBuzz function in Java
    rev1

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -19,3 +19,34 @@ class Solution {
 
     }
 }
+
+// or
+
+import java.util.*;
+public class Main {
+  
+  public static List<String> fizzBuzz(int n){
+    List<String> res = new ArrayList<>();
+    for(int i = 1; i<=n; i++){
+      if(i%3 == 0 && i %5 ==0){
+        res.add("FizzBuzz");
+      }
+      else if(i%3==0){
+        res.add("Fizz");
+      }
+      else if (i%5==0){
+        res.add("Buzz");
+      }
+      else{
+        res.add(String.valueOf(i));
+      }
+    }
+    return res;
+    
+  }
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      System.out.println(fizzBuzz(n));
+    }
+}
