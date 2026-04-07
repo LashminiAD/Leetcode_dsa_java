e6c4853c517cc88391fde8526244924d35e8b287
Lashmini Anand <sec23ci058@sairamtap.edu.in>
2026-04-08 05:07:21 +0530

    Refactor prime counting in count-primes.java
    Refactor prime counting logic and simplify code structure.

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -15,19 +15,41 @@ class Solution {
             }
         }
         int count = 0;
-        for(int i =2; i<n; i++){
-            if(isPrime[i]){
-                count++;
+        for(int i =2; i<n; i++){  //  or  for(boolean c : isPrime) {
+            if(isPrime[i]){      //             if(c) count++;
+                count++;        //         return count; }
             }
         }
         return count;
-
-    //or 
-        int count = 0;
-    for(boolean c : isPrime){
-        if(c) count++;
     }
-    return count;
 }
+
+//or
+
+import java.util.*;
+class Main {
+    
+    static boolean isPrime(int n){
+        if(n<=1) return false;
+        for(int i =2; i*i<=n; i++){
+            if(n%i==0) return false;
+        }
+        return true;
+    }
+        static int primeNum(int n){
+            int c = 0;
+            for(int i =2; i<n; i++){
+                if(isPrime(i)){
+                    c++;
+                }
+            }
+            return c;
+        }
+        
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(primeNum(n));
+    
     }
 }
