9b91890e72b38dc7cc7cbd447a1103ab1fa3c188
Lashmini Anand <sec23ci058@sairamtap.edu.in>
2026-04-24 14:06:45 +0530

    Refactor groupAnagrams to use sorting method rev 1
    Refactored groupAnagrams method to include sorting approach for grouping anagrams.

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,11 +1,13 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String, List<String>> map = new HashMap<>();
+
         for(String s : strs){
             int []count = new int[26];
             for(char c : s.toCharArray()){
                 count[c-'a']++;
             }
+            
             String cs = Arrays.toString(count);
             map.putIfAbsent(cs, new ArrayList<>());
             map.get(cs).add(s);
@@ -13,3 +15,16 @@ class Solution {
         return new ArrayList<>(map.values());
     }
 }
+// or O(n * k log k) SORTING APPORACH
+        for (String s : strs) {
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            
+            String cs = new String(arr);
+//or containsKey 
+            if(!map.containsKey(cs)) {
+                map.put(cs, new ArrayList<>());  }
+// cleaner way instead of containsKey we can use putIfAbsent      
+            map.putIfAbsent(cs, new ArrayList<>());
+            map.get(cs).add(s);
+        }
