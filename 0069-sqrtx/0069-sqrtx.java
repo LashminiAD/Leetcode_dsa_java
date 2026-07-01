36e63d8735e6f0733292f67620346a5db30da79d
Lashmini Anand <sec23ci058@sairamtap.edu.in>
2026-07-01 06:28:26 +0530

    Time: 1 ms (97.72%), Space: 42.5 MB (61.19%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,28 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int) Math.sqrt(x);
+        // return (int) Math.sqrt(x);
+        int low = 0;
+        int high = x;
+        int ans = 0;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            long square = (long) mid * mid;
+
+            if (square == x) {
+                return mid;
+            } 
+            else if (square < x) {
+                ans = mid;
+                low = mid + 1;
+            } 
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
     }
 }
