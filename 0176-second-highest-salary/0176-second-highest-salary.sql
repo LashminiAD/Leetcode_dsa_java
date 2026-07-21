5667f442d675745f5c262ea10ba8491fd11e13a2
Lashmini Anand <sec23ci058@sairamtap.edu.in>
2026-07-21 11:46:59 -0700

    Time: 308 ms (24.73%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,6 +1,7 @@
-select (
-    select distinct salary
-        from Employee
-        order by salary desc
-        limit 1 offset 1
-    )    as SecondHighestSalary;
+-- select (
+--     select distinct salary
+--         from Employee
+--         order by salary desc
+--         limit 1 offset 1
+--     )    as SecondHighestSalary;
+select max(salary) as SecondHighestSalary from Employee where salary not in (select max(salary) from Employee);
